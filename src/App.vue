2e577a3f90a75911d86bf1ba93c0568f02edf953
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2023-04-15 01:01:27 -0700

    Google Log in

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,28 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+// isSignedIn to make sure "LogIn" button goes away if currently logged in
+const isSignedIn = ref(false)
+
+function googleSignin() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    const user = result.user;
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    const email = error.customData.email;
+    const credential = GoogleAuthProvider.credentialFromError(error);
+  });
+}
+
 </script>
 
 <template>
@@ -11,8 +33,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/"> Home </RouterLink>
+        <RouterLink to="/signin"> Login </RouterLink>
+        <button @click="googleSignin"> Login/Sign-up </button>
       </nav>
     </div>
   </header>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 import './assets/main.css'
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqMVnxGikQ4ilzOICIk8dO0Fy-kOvXfPc",
+  authDomain: "rideshare-6c04b.firebaseapp.com",
+  projectId: "rideshare-6c04b",
+  storageBucket: "rideshare-6c04b.appspot.com",
+  messagingSenderId: "393013386646",
+  appId: "1:393013386646:web:37ad05621c399a110611b7",
+  measurementId: "G-LMD6SXEJBS"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
+
 
 app.use(router)
 
