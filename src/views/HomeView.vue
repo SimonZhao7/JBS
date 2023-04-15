67b9f7296fc7cfc39c41dce12adf58b080e61550
Bruce Bai <brbai@ucsc.edu>
2023-04-15 12:49:41 -0700

    adjustments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <script setup>
-//import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <div class="imgbox">
-      <!-- <img class="center-fit" src="../../img/IMG_8862.JPG"> -->
+  <html>
+    <TheWelcome />
 
-      <div class="header">
-        <div class="txt">
-          <h1>JBS</h1>
-        </div>
-      </div>
-    </div>
-    <!-- <TheWelcome /> -->
-  </main>
+    <main>
+        <head>
+          <title>FPS</title>
+        </head>
+
+        <body>
+          <p style="background-image: url(../../img/IMG_8862.JPG);"></p>
+        </body>
+    </main>
+  </html>
 </template>
+
+<style>
+
+</style>
