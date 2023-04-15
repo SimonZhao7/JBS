f244cf43a2c8eabd470e6444e1cf86cf686bee4c
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-04-15 13:54:42 -0700

    Input Components

---
+++ src/components/Input.vue
@@ -0,0 +1,30 @@
+<script setup>
+    const emit = defineEmits(['change'])
+    const props = defineProps({
+        value: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        placeHolder: {
+            type: String,
+            default: '',
+        },
+        labelText: {
+            type: String,
+            default: '',
+        },
+    })
+</script>
+
+<template>
+    <div>
+        <label v-if="labelText.length > 0" class="mb-2">
+            {{ labelText }}
+        </label>
+        <input class="w-full h-10 border-gray-300 border-2 rounded-md transition-all duration-150 outline-none px-4 hover:border-gray-600 focus:border-gray-600" :type="type" :value="value" @input="(e) => emit('change', e.target.value)" :placeholder="placeHolder"/>
+    </div>
+</template>
