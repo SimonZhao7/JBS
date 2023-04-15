c0ac00eab5d1da0adabd76856f280f0e58678e8a
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-04-15 01:07:58 -0700

    Fix Router Bug

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const router = createRouter({
       name: 'signin',
       component: () => import("../views/signin.vue")
     },
-    {
-      path: '/feed',
-      name: 'feed',
-      component: () => import("../views/feed.vue")
-    }
   ]
 })
 
