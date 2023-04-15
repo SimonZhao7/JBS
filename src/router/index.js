486d6bb5a68c6bd2f56b666fa0b8dd34c48c11fa
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2023-04-15 01:12:17 -0700

    min fixes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignIn from '../views/signin.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,8 +13,8 @@ const router = createRouter({
     {
       path: '/signin',
       name: 'signin',
-      component: () => import("../views/signin.vue")
-    },
+      component: SignIn
+    }
   ]
 })
 
