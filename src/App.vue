dcef713edda902815b6c190a1d027ff65784d63d
JiaCheng Liu <85423274+JiaCheng2004@users.noreply.github.com>
2023-04-16 02:45:13 -0700

    Info page style + fix navbar

--- src/App.vue
+++ src/App.vue
@@ -54,17 +54,14 @@ function redirectAddTrips() {
 
 <template>
   <header>
-    <nav class="shadow-md h-20 w-full py-3 text-md bg-white rounded-b-[20px] fixed">
+    <nav class="shadow-md h-20 w-full py-3 text-md bg-white rounded-b-[20px] fixed md:px-10 sm: px-10">
       <div class="flex items-center justify-between max-w-7xl mx-auto w-full h-full text-lg space-x-8">
         <div class="flex space-x-8">
-          <div class="hover:text-gray-500 cursor-pointer">
+          <div class="hover:text-gray-500 cursor-pointer ">
             <RouterLink to="/"> Home </RouterLink>
           </div>
-          <div @click="googleSignin" class="hover:text-gray-500 cursor-pointer">
-            Login
-          </div>
+            <div @click="googleSignin" class="hover:text-gray-500 cursor-pointer">Login</div>
         </div>
-        
         <button class="shw-btn border-0 rounded-full w-12 h-12 bg-white text-3xl flex items-center justify-center hover:scale-110 transition-all duration-150 text-gray-600" @click="redirectAddTrips" >+</button>
       </div>
     </nav>
